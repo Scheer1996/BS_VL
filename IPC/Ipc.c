a0434fc2f09c11856110bf3984cea68f8dc65036
Philip Scheer <philip@philip-scheer.de>
2017-06-27 10:36:08 +0200

    IPC

---
+++ IPC/Ipc.c
@@ -0,0 +1,54 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <semaphore.h>
+
+#define PUFFER_SIZE 10
+
+	pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+	sem_t b;
+	sem_t f;
+
+
+void* erzeuger( void* notused ) {
+	while(1) {
+		sem_wait(&f);					//f - 1  sonst warten
+		pthread_mutex_lock(&mutex);
+		printf("Erzeuger:    Element hinzugefügt\n");
+		pthread_mutex_unlock(&mutex);
+		sem_post(&b);					//b + 1
+
+		int b_val;
+		int f_val;
+		sem_getvalue(&b, &b_val);
+		sem_getvalue(&f, &f_val);
+		printf("Erzeuger:    B: %d, F: %d \n", b_val, f_val);
+
+		sleep(1);
+	}
+}
+
+
+void* verbraucher( void* notused ) {
+	while(1) {
+		sem_wait(&b);				// b - 1 sonst warten
+		pthread_mutex_lock(&mutex);
+		printf("Verbraucher: Element entfernt \n");
+		pthread_mutex_unlock(&mutex);
+		sem_post(&f);				// f + 1 sonst waren
+		sleep(5);
+	}
+}
+
+
+
+int main() {
+	pthread_mutex_init(&mutex, NULL);
+	sem_init(&f, 0, PUFFER_SIZE);
+	sem_init(&b, 0, 0);
+	pthread_t dummy;
+	pthread_create(&dummy, NULL, erzeuger, NULL);
+	//pthread_create(&dummy, NULL, verbraucher, NULL);
+
+	sleep(60);
+	return 0;
+}
---
+++ IPC/Ipc.h
@@ -0,0 +1,13 @@
+/*
+ * ipc.h
+ *
+ *  Created on: 27.06.2017
+ *      Author: Philip
+ */
+
+#ifndef IPC_IPC_H_
+#define IPC_IPC_H_
+
+
+
+#endif /* IPC_IPC_H_ */
